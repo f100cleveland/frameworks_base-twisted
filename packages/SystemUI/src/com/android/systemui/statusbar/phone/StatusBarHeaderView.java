047e2b9d3fd779bd015b5ffd4aca8b2eb90ef7e7
maxwen <max.weninger@gmail.com>
2015-08-20 22:06:06 +0200

    SystemUI: time-context header fixes
    only run updates when screen is on
    
    Change-Id: I8e6775164839df59dfc9a077de656a7917637859

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -43,6 +43,7 @@ import android.provider.CalendarContract.Events;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.MathUtils;
 import android.util.TypedValue;
 import android.view.View;
