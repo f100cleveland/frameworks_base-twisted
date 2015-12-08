98f633c0750706ff10a9801e5eed43c5bee4fecb
xWASABI <wasabi.dev@gmail.com>
2015-12-08 15:14:56 -0500

    Dark Notifications
    Part 2 of my first commit which is really just a slight variation of
    
    https://github.com/TeamTwisted/platform_frameworks_base/commit/78368ca264fa01e8a07dbb572989b1733d2083dc and https://github.com/TeamTwisted/platform_frameworks_base/commit/4885e4e162adc5d16b99e31ce72ce7e1efddb6ca and https://github.com/TeamTwisted/platform_frameworks_base/commit/157cb721a551fcc10b53f27bf07a7f41c34e8b1c
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6186,7 +6186,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             PackageManager.FEATURE_TELEVISION)) {
                         theme = com.android.internal.R.style.Theme_Leanback_Dialog_Alert;
                     } else {
-                        theme = 5;
+                        theme = 6;
                     }
 
                     mBootMsgDialog = new ProgressDialog(mContext, theme) {
