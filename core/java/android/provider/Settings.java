09235c8d03e26a0818a141cf3933645b35e988c8
Alex Cruz <mazdarider23@gmail.com>
2016-04-10 16:37:49 -0400

    Dashoard tileview dividers toggle [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3399,6 +3399,12 @@ public final class Settings {
         public static final String DASHBOARD_TILEVIEW_DOUBLE_LINES = "dashboard_tileview_double_lines";
 
         /**
+         * Whether to display dashboard dividers
+         * @hide
+         */
+        public static final String DASHBOARD_TILEVIEW_DIVIDERS = "dashboard_tileview_dividers";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
